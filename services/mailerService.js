8a440bbf463099bfcbf475730c1de84d0bedc368
Felix <felix.bongo@tweeklabs.com>
2020-12-01 17:29:09 +0800

    update

--- services/mailerService.js
+++ services/mailerService.js
@@ -1,15 +1,16 @@
 const nodemailer = require("nodemailer");
 
-exports.contact = function(data) {
+exports.contact = async function(data) {
 console.log('called', data);
 
     // test start
 
 // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: true, // true for 465, false for other ports
+    // host: process.env.MAIL_HOST,
+    // port: process.env.MAIL_PORT,
+    // secure: true, // true for 465, false for other ports
+    service: 'gmail',
     auth: {
       user: process.env.MAIL_USERNAME, // generated ethereal user
       pass: process.env.MAIL_PASSWORD, // generated ethereal password
@@ -17,10 +18,10 @@ console.log('called', data);
     });
   
 //   send mail with defined transport object
-    return transporter.sendMail({
+    return await transporter.sendMail({
     from: process.env.MAIL_FROM, // sender address
     to: process.env.MAIL_TO, // list of receivers
-    subject: "New Contact from client", // Subject line
+    subject: "New Contact from client - " + data.name, // Subject line
     html: `<p>Hi Admin,</p>
             <p>New Contact Us Inquiry:</p>
             <p>From: ${data.email}<br>
