c30e97c26137eaab4245d38784afce08313324d1
Felix <felix.bongo@tweeklabs.com>
2021-03-02 16:35:04 +0800

    switch chromium dev-prod

--- crawler/browser.js
+++ crawler/browser.js
@@ -4,12 +4,25 @@ async function startBrowser(){
     let browser;
     try {
         console.log("Opening the browser......");
-        browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            'ignoreHTTPSErrors': true,
-            executablePath: '/usr/bin/chromium-browser'
-        });
+
+        var ENV = process.env.ENVIRONMENT || 'prod'; 
+        console.log(ENV);
+
+        if ( ENV === "prod" ) {
+            browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+                'ignoreHTTPSErrors': true,
+                executablePath: '/usr/bin/chromium-browser'
+            });
+        } else {
+            browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+                'ignoreHTTPSErrors': true
+            });
+        }
+        
     } catch (err) {
         console.log("Could not create a browser instance => : ", err);
     }
