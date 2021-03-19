7bda1d7b483adfc884d9d58698ee21e38be87f1a
Bigfoot <felix@trademarkers.com>
2021-03-19 22:41:27 +0800

    update

---
+++ public/assignment/js/custom.js
@@ -0,0 +1,10 @@
+$.ajax('http://api.bigfoot.com/comm', 
+{
+    dataType: 'json', // type of response data
+    success: function (data,status,xhr) {   // success callback function
+        console.log(data);
+    },
+    error: function (jqXhr, textStatus, errorMessage) { // error callback 
+        console.log(errorMessage);
+    }
+});
