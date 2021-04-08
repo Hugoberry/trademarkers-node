37edf5e698c09c8d0ff7e94ecf1eed7883008d55
Felix <felix.bongo@tweeklabs.com>
2021-04-08 14:35:55 +0800

    update

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -22,7 +22,7 @@ $( document ).ready(function() {
             // } else {
             //   document.location.href = '/customer';
             // }
-            document.location.href = '/customer';
+            // document.location.href = '/customer';
             // document.location.href = '/researcher';
           } else {
             $(".alert-warning strong").html(result.message);
