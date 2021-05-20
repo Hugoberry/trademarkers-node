f51513465ff92a3d1c40af5e3b5411050ee425c6
Felix <felix.bongo@tweeklabs.com>
2021-05-20 14:44:10 +0800

    update

--- services/oaCronService.js
+++ services/oaCronService.js
@@ -31,6 +31,10 @@ for (let i = 0; count < 1 ; i++) {
     let noticeOfAllowanceDateFormatted = helpers.convertIntToDate(rec[i].noticeOfAllowanceDate);
     let deadLine = moment(noticeOfAllowanceDateFormatted).add(6, "M").format("YYYY-MM-DD");
 
+    if ( rec[i].statusDescription == "A first request for extension of time to file a Statement of Use has been granted." ) {
+      deadLine = moment(noticeOfAllowanceDateFormatted).add(12, "M").format("YYYY-MM-DD");
+    }
+
     // let noaDeadLine = moment(noticeOfAllowanceDateFormatted).diff(deadLine, "months");
     console.log('dead date', moment(deadLine).diff(moment(), "weeks"));
     
